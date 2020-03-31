deaeb7bb44d87afb598f0559d7845d2f9776d388
Sinkerine <git@15cm.net>
2020-03-30 23:17:02 -0700

    Update with Ergodox ez config

--- keyboards/ergodox_ez/keymaps/15cm/config.h
+++ keyboards/ergodox_ez/keymaps/15cm/config.h
@@ -2,8 +2,10 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
+#define ORYX_CONFIGURATOR
 #undef DEBOUNCE
 #define DEBOUNCE 5
 
 #undef TAPPING_TERM
 #define TAPPING_TERM 150
+
