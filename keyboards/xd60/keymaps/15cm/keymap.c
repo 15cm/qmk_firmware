de623c8140efcbf2ffc6a0b7e5725cf7a66d405c
Sinkerine <git@15cm.net>
2020-03-30 23:08:38 -0700

    Update XD60 and add layer3 for Ergodox

--- keyboards/xd60/keymaps/15cm/keymap.c
+++ keyboards/xd60/keymaps/15cm/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB  , KC_Q   , KC_W   , KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS, \
         F_ESC   , KC_A   , KC_S   , KC_D, KC_F, KC_G, KC_H, KC_J, KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_NO, KC_ENT , \
                 KC_LSFT , KC_NO, KC_Z   , KC_X   , KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_RSFT, KC_RSFT, \
-        F_CTL   , KC_LALT, F_LGUI,                F_SPC              ,    KC_RGUI, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT),
+        F_CTL   , KC_LALT, F_LGUI,                KC_SPC              ,    KC_RGUI, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT),
 
     /*
         1 char = 0.25u
